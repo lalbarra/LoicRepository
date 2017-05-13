3c2b426aa1d2c92abd618927f72c3afb98fa3005
lalbarra <loic.albarracin@gmail.com>
2017-05-13 15:21:39 +0200

    Interface de forain

--- Sudo_Clue/app/src/main/java/com/example/loic/sudo_clue/MainActivity.java
+++ Sudo_Clue/app/src/main/java/com/example/loic/sudo_clue/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     boolean eraser = false;
     Button[][] listButtons = new Button[9][9];
     int[][] matrice = new int[9][9];
+    Button [] listLastButtons = new Button [2];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
                 GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
                 GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(rowSpan, colSpan);
                 Button button = new Button(this);
-                getAreaColored(i, j, button);
-                button.getResources().getDrawable(R.drawable.button_border);
+                GradientDrawable gd = new GradientDrawable();
+                gd.setCornerRadius(5);
+                gd.setStroke(1, 0xFF000000);
+                getAreaColored(i, j, gd);
+                button.setBackgroundDrawable(gd);
                 button.setLayoutParams(gridParam);
                 matrice[i][j] = 0;
-
                 // Set the action when you push a button
                 listButtons[i][j] = button ;
                 button.setOnClickListener(new View.OnClickListener() {
@@ -65,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-        // Button in the bottom of the grid to fill the grid
+        // Button in the bottom to fill the grid
+
         for (int j = 0; j < 9; j++) {
             final int z = j+1;
             GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
@@ -74,21 +78,22 @@ public class MainActivity extends AppCompatActivity {
             Button button = new Button(this);
             button.setText("" + (j+1));
             button.setLayoutParams(gridParam);
+ // Set Action On Click : if push, erase disabled and fill with the number which is indicated
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     myInt = z;
+                    if (eraser){
+                        eraser = false;
+                    }
                 }
             });
-            GradientDrawable gd = new GradientDrawable();
-            gd.setColor(0xFF00FF00); // Changes this drawable to use a single color instead of a gradient
-            gd.setCornerRadius(5);
-            gd.setStroke(1, 0xFF000000);
-            button.setBackgroundDrawable(gd);
+
             gridLayout.addView(button, gridParam);
         }
         // Solver and Eraser
         for (int j = 0; j < 2; j++) {
+            final int z= j ;
             GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
             GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
             GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(rowSpan, colSpan);
@@ -96,14 +101,19 @@ public class MainActivity extends AppCompatActivity {
             if (j == 0) {
                 button.setText("Eraser");
                 button.setLayoutParams(gridParam);
+
+                //Set the actions of Eraser
+                listLastButtons[j] = button;
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                          if (eraser){
                              eraser = false;
+                             listLastButtons[z].setSelected(false);
                          }
                          else {
                              eraser = true;
+                             listLastButtons[z].setSelected(true);
                          }
                       }
                   }
@@ -124,42 +134,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void getAreaColored(int i, int j, Button jb) {
+    public void getAreaColored(int i, int j, GradientDrawable gd) {
         // Put color even if editText is empty
-        if(("").equals(jb.getText().toString())) {
+        //if(("").equals(gd.getText().toString())) {
             //Three First Columns
             if ((i >= 0 && i < 3) && (j >= 0 && j < 3)) {
-                jb.setBackgroundResource(R.color.green);
+                gd.setColor(Color.RED);
             }
             if ((i >= 3 && i < 6) && (j >= 0 && j < 3)) {
-                jb.setBackgroundColor(getResources().getColor(R.color.brown));
+                gd.setColor(getResources().getColor(R.color.brown));
             }
             if ((i >= 6 && i < 9) && (j >= 0 && j < 3)) {
-                jb.setBackgroundColor(Color.BLUE);
+                gd.setColor(Color.BLUE);
             }
 
             //Three Second Column
             if ((i >= 0 && i < 3) && (j >= 3 && j < 6)) {
-                jb.setBackgroundColor(Color.CYAN);
+                gd.setColor(Color.CYAN);
             }
             if ((i >= 3 && i < 6) && (j >= 3 && j < 6)) {
-                jb.setBackgroundColor(Color.WHITE);
+                gd.setColor(Color.WHITE);
             }
             if ((i >= 6 && i < 9) && (j >= 3 && j < 6)) {
-                jb.setBackgroundColor(Color.YELLOW);
+                gd.setColor(Color.YELLOW);
             }
 
             //Three Third Column
             if ((i >= 0 && i < 3) && (j >= 6 && j < 9)) {
-                jb.setBackgroundColor(Color.GREEN);
+                gd.setColor(Color.GREEN);
             }
             if ((i >= 3 && i < 6) && (j >= 6 && j < 9)) {
-                jb.setBackgroundColor(Color.GRAY);
+                gd.setColor(Color.GRAY);
             }
             if ((i >= 6 && i < 9) && (j >= 6 && j < 9)) {
-                jb.setBackgroundColor(Color.MAGENTA);
+                gd.setColor(Color.MAGENTA);
             }
-        }
+        //}
     }
 
 
